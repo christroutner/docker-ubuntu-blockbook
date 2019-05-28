b080ec4375acd56346cc735cfa2b49d0e15f60b8
Chris Troutner <chris.troutner@gmail.com>
2019-05-28 09:45:50 -0700

    Setting container to restart automatically

--- run-container.sh
+++ run-container.sh
@@ -2,8 +2,9 @@
 
 # This script starts the Blockbook indexer inside the Docker container.
 
-docker container run --rm -d --name dev-bb \
+docker container run -d --name dev-bb \
 -v /home/safeuser/docker-ubuntu-blockbook/data:/opt/coins/data/bcash/blockbook/db \
 -v /home/safeuser/docker-ubuntu-blockbook/logs:/opt/coins/blockbook/bcash/logs \
 -p 9130:9130 -p 9030:9030 \
+--restart=always \
 dev-bb
